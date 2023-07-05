28eab3f77eebcdf9a3a8c667248a9353a30aa4b9
sejalv1 <124467414+sejalv1@users.noreply.github.com>
2023-07-06 00:42:41 +0530

    message hello

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div><h1 className='heading' contentEditable = "false">Hello world</h1></div>
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
